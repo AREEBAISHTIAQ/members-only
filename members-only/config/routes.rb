6b5feededd558ca1373dc83edad2191ea9e2460b
AREEBAISHTIAQ <areeba.aic@gmail.com>
2018-08-12 17:33:34 +0500

    finish basic setup

--- members-only/config/routes.rb
+++ members-only/config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
+  root 'static_pages#home'
   get 'static_pages/posts'
+  get 'sessions/new'
   get '/signup', to: 'users#new'
   post '/signup', to: 'users#create'
 end
--- members-only/test/controllers/static_pages_controller_test.rb
+++ members-only/test/controllers/static_pages_controller_test.rb
@@ -4,11 +4,13 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get static_pages_home_url
     assert_response :success
+    assert_select "title", "Home | WRITEAPOST"
   end
 
   test "should get posts" do
     get static_pages_posts_url
     assert_response :success
+    assert_select "title", "Posts | WRITEAPOST"
   end
 
 end
