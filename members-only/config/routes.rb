0839308edd0e60aae572eace6432622097b3db14
AREEBAISHTIAQ <areeba.aic@gmail.com>
2018-08-13 19:59:15 +0500

    Finish sessions

--- members-only/config/routes.rb
+++ members-only/config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   root 'static_pages#home'
   get '/posts',    to: 'static_pages#posts'
   get  '/signup',  to: 'users#new'
+  post '/signup',  to: 'users#create'
+  resources :users
 end
